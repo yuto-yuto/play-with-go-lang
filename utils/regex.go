bd480d5e93bda22d2863f2fd29badefea896d4c6
yuto-yuto <yuto.nakatsu@gmail.com>
2023-03-21 15:20:50 +0000

    add regex

---
+++ utils/regex.go
@@ -0,0 +1,133 @@
+package utils
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func RunRegex() {
+	runMatch()
+	panicCompile()
+	useQuoteMeta()
+	fmt.Println()
+	runFindSomething()
+}
+
+func runMatch() {
+	reg, err := regexp.Compile("ID_\\d")
+	if err != nil {
+		fmt.Println("error in regex string")
+	}
+
+	fmt.Println(reg.Match([]byte("ID_1")))   // true
+	fmt.Println(reg.Match([]byte("ID_ONE"))) // false
+
+	fmt.Println(reg.MatchString("ID_1"))   // true
+	fmt.Println(reg.MatchString("ID_ONE")) // false
+
+}
+
+func runFindSomething() {
+	reg, _ := regexp.Compile(`ID_\d`)
+
+	text := "ID_1, ID_42, RAW_ID_52, "
+	fmt.Printf("%q\n", reg.FindAllString(text, 2)) // ["ID_1" "ID_4"]
+	fmt.Printf("%q\n", reg.FindAllString(text, 5)) // ["ID_1" "ID_4" "ID_5"]
+
+	result := reg.FindAllString(text, -1)
+	fmt.Printf("%q\n", result)               // ["ID_1" "ID_4" "ID_5"]
+	fmt.Printf("%q\n", result[2])            // "ID_5"
+	fmt.Printf("%q\n", reg.FindString(text)) // "ID_1"
+
+	fmt.Printf("%q\n", reg.FindAllStringSubmatch(text, -1)) // [["ID_1"] ["ID_4"] ["ID_5"]]
+
+	fmt.Println("--- submatch ---")
+
+	reg2, err := regexp.Compile(`([^,\s]+)_(\d+)`)
+	if err != nil {
+		fmt.Println("error in regex string")
+	}
+
+	fmt.Printf("%q\n", reg2.FindAllString(text, -1)) // ["ID_1" "ID_42" "RAW_ID_52"]
+	result2 := reg2.FindAllStringSubmatch(text, -1)
+	fmt.Printf("%q\n", result2) // [["ID_1" "ID" "1"] ["ID_42" "ID" "42"] ["RAW_ID_52" "RAW_ID" "52"]]
+	fmt.Printf("Key: %s, ID: %s\n", result2[2][1], result2[2][2])
+}
+
+func panicCompile() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("panic: ", r)
+		}
+	}()
+
+	regexp.MustCompile("ID_\\p")
+}
+
+func useQuoteMeta() {
+	_, err := regexp.Compile(regexp.QuoteMeta("ID_\\p"))
+	if err != nil {
+		fmt.Println("error in regex string")
+	}
+}
+
+func runAdvanced() {
+	texts := []string{
+		`\PRESENT\1\KEY1: 11`,
+		`\PRESENT\1\KEY2: "value-2"`,
+		`\PRESENT\1\KEY22: ""`,
+		`\PRESENT\1\0\KEY3: 22`,
+		`\PRESENT\1\0\KEY4: "value-4`,
+	}
+
+	// regex := regexp.MustCompile(`(\\PRESENT\\1\\([A-Za-z0-9]+)|\\PRESENT\\1\\[0-9]+\\([A-Za-z0-9]+)): (([0-9]*)|"(.*)")`)
+	// regex := regexp.MustCompile(`(\\PRESENT\\1\\([A-Za-z0-9]+)|\\PRESENT\\1\\[0-9]+\\([A-Za-z0-9]+)): ("([^"]*)"|([0-9]*))`)
+	// regex := regexp.MustCompile(`\\PRESENT\\1\\([A-Za-z0-9]+): (([0-9]*)|"([^"]*)")`)
+	// regex := regexp.MustCompile(`\\PRESENT\\1\\([A-Za-z0-9]+): ("([^"]*)"|([0-9]*))`)
+	prefix := `\\PRESENT\\1`
+	regexString := fmt.Sprintf(`%s\\[0-9\\]*([A-Za-z0-9]+): "?([^"]*)"?`, prefix)
+	regex, err := regexp.Compile(regexString)
+	// regex, err := regexp.Compile(`\\PRESENT\\1\\[0-9\\]*([A-Za-z0-9]+): "?([^"]*)"?`)
+	if err != nil {
+		fmt.Println(regexString)
+		fmt.Println(err)
+		return
+	}
+
+	for _, text := range texts {
+		fmt.Println(regex.FindAllString(text, -1))
+	}
+
+	for _, text := range texts {
+		result := regex.FindStringSubmatch(text)
+		fmt.Printf("FindStringSubmatch (len: %d): %+v\n", len(result), result)
+		for index, match := range result {
+			fmt.Printf("%d: %+v\n", index, match)
+		}
+
+		allResult := regex.FindAllStringSubmatch(text, -1)
+		fmt.Printf("FindAllStringSubmatch(len: %d): %+v\n", len(allResult), allResult)
+		for index, match := range allResult {
+			fmt.Printf("%d: %+v\n", index, match)
+		}
+		fmt.Println()
+	}
+
+	pre := `\\\\PRESENT\\\\1`
+	// regStr := fmt.Sprintf(`%s\\\\([A-Za-z0-9]+)": "?([^"]*)"?`, pre)
+	regStr := fmt.Sprintf(`%s\\\\([0-9]*)\\*([^\\]+)": "?([^"]*)"?`, pre)
+	reg := regexp.MustCompile(regStr)
+	// fmt.Println(regStr)
+	keys := []string{
+		"{\n\"\\\\FOLDER\\\\PRESENT\\\\1\\\\NUMBER\": 1",
+		"{\n\"\\\\FOLDER\\\\PRESENT\\\\1\\\\0\\\\NUMBER\": 1",
+	}
+	for _, key := range keys {
+		fmt.Println(reg.MatchString(key))
+		result := reg.FindStringSubmatch(key)
+		fmt.Println(len(result))
+		for index, data := range result {
+			fmt.Printf("%d: %s\n", index, data)
+		}
+	}
+}
