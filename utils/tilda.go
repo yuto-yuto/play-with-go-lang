5266e851de5bac7c112414928b824976062c339c
yuto-yuto <yuto.nakatsu@gmail.com>
2023-05-26 07:50:46 +0000

    Update tilda

--- utils/tilda.go
+++ utils/tilda.go
@@ -13,20 +13,38 @@ type UnsignedIntWithTilda interface {
 	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
+type UintAlias = uint
 type MyUnsignedInt uint
 
 func RunTilda() {
-	acceptUnsignedInt(uint(1))
-	acceptUnsignedInt(uint8(1))
-	acceptUnsignedInt(uint64(1))
-	// MyUnsignedInt does not implement UnsignedInt (possibly missing ~ for uint in constraint UnsignedInt)compilerInvalidTypeArg
-	// acceptUnsignedInt(MyUnsignedInt(1))
+	acceptUint(uint(1))
+	acceptUint32(uint32(1))
+	acceptUint64(uint64(1))
+	acceptAny(uint32(22))    // 22
+	acceptAny("sample text") // unsupported data type: string
+
+	fmt.Println()
+	acceptUintGenerics(uint(5))
+	acceptUintGenerics(uint8(6))
+	acceptUintGenerics(uint16(7))
+	acceptUintGenerics(uint32(8))
+	acceptUintGenerics(uint64(9))
+	// acceptUintGenerics("sample")
 
 	fmt.Println()
 
 	acceptUnsignedInt(uint(1))
 	acceptUnsignedInt(uint8(1))
 	acceptUnsignedInt(uint64(1))
+	acceptUnsignedInt(UintAlias(1))
+	// MyUnsignedInt does not implement UnsignedInt (possibly missing ~ for uint in constraint UnsignedInt)compilerInvalidTypeArg
+	// acceptUnsignedInt(MyUnsignedInt(1))
+
+	fmt.Println()
+
+	acceptUnsignedIntWithTilda(uint(1))
+	acceptUnsignedIntWithTilda(uint8(1))
+	acceptUnsignedIntWithTilda(uint64(1))
 	acceptUnsignedIntWithTilda(MyUnsignedInt(1))
 
 	fmt.Println()
@@ -39,6 +57,35 @@ func RunTilda() {
 		Weight: 3.2,
 	}
 	fmt.Println(product)
+	fmt.Println(product.Height)
+	fmt.Println(product.Width)
+}
+
+func acceptUint(value uint) {
+	fmt.Println(value)
+}
+func acceptUint32(value uint32) {
+	fmt.Println(value)
+}
+func acceptUint64(value uint64) {
+	fmt.Println(value)
+}
+
+func acceptAny(value any) {
+	switch v := value.(type) {
+	case uint:
+		fmt.Println(v)
+	case uint32:
+		fmt.Println(v)
+	case uint64:
+		fmt.Println(v)
+	default:
+		fmt.Printf("unsupported data type: %T", v)
+	}
+}
+
+func acceptUintGenerics[T uint | uint8 | uint16 | uint32 | uint64](value T) {
+	fmt.Println(value)
 }
 
 func acceptUnsignedInt[T UnsignedInt](value T) {
@@ -52,13 +99,13 @@ func acceptUnsignedIntWithTilda[T UnsignedIntWithTilda](value T) {
 type Centimeter float64
 
 func (value Centimeter) String() string {
-	return fmt.Sprintf("%s cm", toStringWithTwoDigitsAfterDecimalPoint(value))
+	return fmt.Sprintf("%s cm", toStringWithTwoDigitsAfterDecimalPoint(value, 1))
 }
 
 type Kilogram float64
 
 func (value Kilogram) String() string {
-	return fmt.Sprintf("%s kg", toStringWithTwoDigitsAfterDecimalPoint(value))
+	return fmt.Sprintf("%s kg", toStringWithTwoDigitsAfterDecimalPoint(value, 2))
 }
 
 type productInTilda struct {
@@ -70,13 +117,10 @@ type productInTilda struct {
 }
 
 func (p productInTilda) String() string {
-	return fmt.Sprintf("(Name: %s, Height: %s, Width: %s, Length: %s, Weight: %s)", p.Name, p.Height.String(), p.Width.String(), p.Length.String(), p.Weight.String())
+	return fmt.Sprintf("(Name: %s, Height: %s, Width: %s, Length: %s, Weight: %s)",
+		p.Name, p.Height, p.Width, p.Length, p.Weight)
 }
 
-type Float64Value interface {
-	~float32 | ~float64
-}
-
-func toStringWithTwoDigitsAfterDecimalPoint[T Float64Value](value T) string {
-	return strconv.FormatFloat(float64(value), 'f', 2, 64)
+func toStringWithTwoDigitsAfterDecimalPoint[T ~float64](value T, digit int) string {
+	return strconv.FormatFloat(float64(value), 'f', digit, 64)
 }
