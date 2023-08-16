c22e72db0646b4c90dd8e587ea9648ff22ed48f9
yuto-yuto <yuto.nakatsu@gmail.com>
2023-08-16 23:39:38 +0000

    update pointer copy

--- utils/pointercopy_test.go
+++ utils/pointercopy_test.go
@@ -135,6 +135,24 @@ func BenchmarkAppendWithSize(b *testing.B) {
 // BenchmarkAppendWithoutSize-12           203014581               18.64 ns/op
 // BenchmarkAppendWithSize-12              1000000000               8.135 ns/op
 
+// vscode ➜ /workspaces/play-with-go/utils (main) $ go test -bench .
+// goos: linux
+// goarch: amd64
+// pkg: play-with-go-lang/utils
+// cpu: Intel(R) Core(TM) i5-7200U CPU @ 2.50GHz
+// BenchmarkBigDataNewValue-4              1000000000               0.3775 ns/op
+// BenchmarkBigDataValueCopy-4             1000000000               0.3333 ns/op
+// BenchmarkBigDataNewPointer-4            1000000000               0.3451 ns/op
+// BenchmarkBigDataPointerCopy-4           1000000000               0.3327 ns/op
+// BenchmarkPairNewValue-4                 1000000000               0.3370 ns/op
+// BenchmarkPairValueCopy-4                1000000000               0.3433 ns/op
+// BenchmarkPairNewPointer-4               1000000000               0.3312 ns/op
+// BenchmarkPairPointerCopy-4              1000000000               0.4139 ns/op
+// BenchmarkAppendWithoutSize-4            50692158               123.3 ns/op
+// BenchmarkAppendWithSize-4       		500221972              111.4 ns/op
+// PASS
+// ok      play-with-go-lang/utils 65.758s
+
 // vscode ➜ /workspaces/play-with-go-lang/utils (main) $ go test -bench . -benchmem
 // goos: linux
 // goarch: amd64
@@ -152,3 +170,21 @@ func BenchmarkAppendWithSize(b *testing.B) {
 // BenchmarkAppendWithSize-12              1000000000               2.059 ns/op           8 B/op          0 allocs/op
 // PASS
 // ok      play-with-go-lang/utils 5.857s
+
+// vscode ➜ /workspaces/play-with-go/utils (main ✗) $ go test -bench . -benchmem
+// goos: linux
+// goarch: amd64
+// pkg: play-with-go-lang/utils
+// cpu: Intel(R) Core(TM) i5-7200U CPU @ 2.50GHz
+// BenchmarkBigDataNewValue-4              1000000000               0.3333 ns/op          0 B/op          0 allocs/op
+// BenchmarkBigDataValueCopy-4             1000000000               0.3450 ns/op          0 B/op          0 allocs/op
+// BenchmarkBigDataNewPointer-4            1000000000               0.3873 ns/op          0 B/op          0 allocs/op
+// BenchmarkBigDataPointerCopy-4           1000000000               0.3355 ns/op          0 B/op          0 allocs/op
+// BenchmarkPairNewValue-4                 1000000000               0.3323 ns/op          0 B/op          0 allocs/op
+// BenchmarkPairValueCopy-4                1000000000               0.3319 ns/op          0 B/op          0 allocs/op
+// BenchmarkPairNewPointer-4               1000000000               0.3353 ns/op          0 B/op          0 allocs/op
+// BenchmarkPairPointerCopy-4              1000000000               0.3333 ns/op          0 B/op          0 allocs/op
+// BenchmarkAppendWithoutSize-4            104694237               22.64 ns/op           43 B/op          0 allocs/op
+// BenchmarkAppendWithSize-4               326078538               11.39 ns/op            8 B/op          0 allocs/op
+// PASS
+// ok      play-with-go-lang/utils 10.665s
